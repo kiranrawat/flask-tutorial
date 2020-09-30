277206a3d99a05b55a985078ec9e89583d5bc254
kiranrawat <rawat.kiran111@gmail.com>
2020-09-29 20:43:21 -0400

    added notebook and data files

---
+++ flaskr/recommender.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import numpy as np 
+
+df1 = pd.read_csv('data/tmdb_5000_credits.csv')
+df2 = pd.read_csv('data/tmdb_5000_movies.csv')
+df3 = pd.read_csv('data/ratings_small.csv')
